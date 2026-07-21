afdd076123f613ee842f14b0ba12646e5465675c
Enatielly <enatielly.rosane@gmail.com>
2026-07-21 16:16:40 -0300

    Complete Day 1 Inputs task

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,4 +1,3 @@
-# Write your code below this line 👇
-print("Hello" + " " + "Angela")
-
+# This line of code will take an input using the input() function
+print("Hello" + " " + input("What is your name?") + "!")
 
