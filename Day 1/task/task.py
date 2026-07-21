6e52b6c76eee0c4b623b6d36705baea8299e39c6
Enatielly <enatielly.rosane@gmail.com>
2026-07-21 17:53:50 -0300

    Complete first project (Band Name Generator) and finish Day 1.

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,3 +1,5 @@
-name = "Angela"
-length = len(name)
-print(length)
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?")
+petname = input("What's your pet's name?")
+print("Your band name could be " + city + " " + petname)
+
