88586a05e50627e808c7845665c86cf4afd2af60
Enatielly <enatielly.rosane@gmail.com>
2026-07-29 11:09:04 -0300

    Day 2 - Primitive Data Types
    Added examples of the four primitive data types in Python: Strings, Integers, Floats, and Booleans. Explored the syntax for large integers and decimals.

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,3 +1,24 @@
-# Subscripting
+# Subscripting - (ou indexação) é o ato de acessar um
+# elemento específico dentro de uma sequência (como uma string, uma lista ou uma tupla)
+# apontando para a posição exata dele.
+# Os colchetes [] são o operador de subscripting.
+
 print("Hello"[4])
 
+print("Hello"[-1])
+
+# String
+print("123" + "123")
+
+# Interger = Whole number
+print(123 + 123)
+
+# Large Intergers
+print(123_456_789)
+
+# Float = Floating Point Number
+print(3.14159)
+
+#Boolean
+print(True)
+print(False)
